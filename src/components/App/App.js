c6d4d930aea9e1278680911046dbab23071991b3
boydDuffie <shmeeves@ufl.edu>
2020-04-25 22:28:21 -0400

    added new logo, and created component files for Picture and App

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Picture from '../Picture/Picture';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Picture />
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Picture/Picture.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Picture.css';
+
+//need to get a list of album names
+//implement a means of cycling through the pictures when image is clicked on 
+
+class Picture extends React.Component {
+    render() {
+        return;
+    }
+}
+
+export default Picture;
