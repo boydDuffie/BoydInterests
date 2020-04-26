95d396c8744c20d76b10e8857cc7d047781c13ef
boydDuffie <shmeeves@ufl.edu>
2020-04-26 00:20:08 -0400

    added more images to array, not done yet. Also implemented click-to-refresh

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -4,7 +4,7 @@ import './Album.css';
 //need to get a list of album names
 //implement a means of cycling through the pictures when image is clicked on 
 
-const albumcovers = ['10,000Days', 'AllOutLife', 'AWizardToldMe', 'Baraka', 'CryLittleSister', 'Detox', 'ESCAPE', 'EVERYDAY'];
+const albumcovers = ['10,000Days', 'AllOutLife', 'AWizardToldMe', 'Baraka', 'CryLittleSister', 'Detox', 'ESCAPE', 'EVERYDAY', 'Fable', 'FinallyDead', 'FRIDAY', 'GlissRiffer', 'GTG', 'HamSandwich', 'HardStuck', 'HeavenUpsideDown', 'hell', 'Holiday', 'IGOR', 'ILoveYou,Goodnight.', 'IntoTheTrees', 'iridescence', 'Jellyfish'];
 
 
 class Picture extends React.Component {
@@ -14,7 +14,9 @@ class Picture extends React.Component {
         return(
             <div className="Album">
                 <div className="image-container">
-                    <img src={process.env.PUBLIC_URL + '/albumcovers/' + albumcovers[i] + '.png'} alt={albumcovers[i]} />
+                    <a href="index.html">
+                        <img src={process.env.PUBLIC_URL + '/albumcovers/' + albumcovers[i] + '.png'} alt={albumcovers[i]} />
+                    </a>
                 </div>
                 <h2>{albumcovers[i]}</h2>
             </div>
